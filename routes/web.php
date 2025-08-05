e8886061bb5723d1563797017e7a1fbf02732cad
Shenoda <shenodamakramibrahim@gmail.com>
2025-08-05 13:17:26 +0300

    feat: Apply Route Model Binding

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,17 @@ Route::get('/tasks', function () {
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::get('/tasks/{id}', function ($id) {
-    return view('show', ['task' => Task::findOrFail($id)]);
+Route::get('/tasks/{task}', function (Task $task) {
+    return view('show', [
+        'task' => $task
+    ]);
 })->name('tasks.show');
+
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', [
+        'task' => $task
+    ]);
+})->name('tasks.edit');
 
 Route::post('/tasks', function (Request $request) {
     $data = $request->validate([
@@ -37,3 +45,19 @@ Route::post('/tasks', function (Request $request) {
     return redirect()->route('tasks.show', ['id' => $task->id])
         ->with('success', 'Task created successfully.');
 })->name('tasks.store');
+
+Route::put('/tasks/{task}', function (Task $task, Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['task' => $task->id])
+        ->with('success', 'Task Updated successfully.');
+})->name('tasks.update');
